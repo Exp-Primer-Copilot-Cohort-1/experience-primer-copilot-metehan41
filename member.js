3352d83e1919c87f67b5f46b28ac7c5534f23917
metehan41 <metehansarikaya41@gmail.com>
2024-05-20 23:26:03 +0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById('member');
+    member.innerHTML = 'Member';
+    member.style.fontSize = '20px';
+    member.style.color = 'blue';
+    member.style.fontWeight = 'bold';
+    member.style.textAlign = 'center';
+    member.style.margin = '10px';
+    member.style.padding = '10px';
+    member.style.border = '1px solid black';
+}
